eb3e6c8e7b7f8be5193d29c418ae91ec2bdd665d
abithaa3101-cmyk <abithaa3101@gmail.com>
2026-09-02 01:29:56 +0530

    Update compute.tf

--- baselines/compute.tf
+++ baselines/compute.tf
@@ -8,13 +8,4 @@ resource "google_org_policy_policy" "disable_serial_port" {
     }
   }
 }
-resource "google_org_policy_policy" "require_os_login" {
-  name   = "organizations/${var.org_id}/policies/compute.requireOsLogin"
-  parent = "organizations/${var.org_id}"
 
-  spec {
-    rules {
-      enforce = true
-    }
-  }
-}
